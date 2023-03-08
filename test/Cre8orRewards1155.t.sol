779cc7d786beb8f60b1c0d7cbc592911a13eb643
Sweets Sweetman <sweetmantech@gmail.com>
2023-03-08 09:32:13 -0300

    I add unit tests to verify mint can only be called by MINTER_ROLE.

--- test/Cre8orRewards1155.t.sol
+++ test/Cre8orRewards1155.t.sol
@@ -9,11 +9,26 @@ contract Cre8orRewards1155Test is Test {
     address public constant DEFAULT_OWNER_ADDRESS = address(0x23499);
 
     function setUp() public {
+        vm.prank(DEFAULT_OWNER_ADDRESS);
         rewards = new Cre8orRewards1155("uri/{id}");
     }
 
     function test_Erc1155() public {
         assertEq("uri/{id}", rewards.uri(1));
         assertEq(0, rewards.balanceOf(DEFAULT_OWNER_ADDRESS, 1));
+    }
+
+    function test_mint_revert_nonMinterRole(uint256 amount) public {
+        vm.expectRevert(
+            "ERC1155PresetMinterPauser: must have minter role to mint"
+        );
+        rewards.mint(DEFAULT_OWNER_ADDRESS, 1, amount, "0x0");
+        assertEq(0, rewards.balanceOf(DEFAULT_OWNER_ADDRESS, 1));
+    }
+
+    function test_mint(uint256 tokenId, uint256 amount) public {
+        vm.startPrank(DEFAULT_OWNER_ADDRESS);
+        rewards.mint(DEFAULT_OWNER_ADDRESS, tokenId, amount, "0x0");
+        assertEq(amount, rewards.balanceOf(DEFAULT_OWNER_ADDRESS, tokenId));
     }
 }
