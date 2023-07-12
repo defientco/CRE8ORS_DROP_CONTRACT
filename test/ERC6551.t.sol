a6dff13aefa7fc6032ac8dee10f3ee479f14ed1a
Sweets Sweetman <sweetmantech@gmail.com>
2023-07-12 11:33:17 -0300

    I import AccountGuardian & EntryPoint.

--- test/ERC6551.t.sol
+++ test/ERC6551.t.sol
@@ -10,11 +10,18 @@ import {IERC721A} from "lib/ERC721A/contracts/IERC721A.sol";
 import {IERC2981, IERC165} from "lib/openzeppelin-contracts/contracts/interfaces/IERC2981.sol";
 import {IOwnable} from "../src/interfaces/IOwnable.sol";
 import {ERC6551Registry} from "../src/utils/ERC6551Registry.sol";
+import {Account as ERC6551Account} from "../src/utils/ERC6551Account.sol";
+import {AccountGuardian} from "../src/utils/AccountGuardian.sol";
+import {EntryPoint} from "lib/account-abstraction/contracts/core/EntryPoint.sol";
 
 contract ERC6551Test is DSTest {
     Cre8ors public cre8orsNFTBase;
     Vm public constant vm = Vm(HEVM_ADDRESS);
     ERC6551Registry erc6551Registry = new ERC6551Registry();
+    AccountGuardian guardian = new AccountGuardian();
+    EntryPoint entryPoint = new EntryPoint();
+    ERC6551Account erc6551Implementation =
+        new ERC6551Account(address(guardian), address(entryPoint));
     DummyMetadataRenderer public dummyRenderer = new DummyMetadataRenderer();
     address public constant DEFAULT_OWNER_ADDRESS = address(0x23499);
     address payable public constant DEFAULT_FUNDS_RECIPIENT_ADDRESS =
