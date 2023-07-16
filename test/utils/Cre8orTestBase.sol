08f875973f32f775af1cc529b8735bc9c880fae0
Sweets Sweetman <sweetmantech@gmail.com>
2023-07-16 10:49:01 -0300

    I init Cre8orTestBase.sol for more composable unit tests and less repeated code.

---
+++ test/utils/Cre8orTestBase.sol
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.15;
+
+import {Cre8ors} from "../../src/Cre8ors.sol";
+import {DummyMetadataRenderer} from "./DummyMetadataRenderer.sol";
+import {IERC721Drop} from "../../src/interfaces/IERC721Drop.sol";
+import {IERC721A} from "lib/ERC721A/contracts/IERC721A.sol";
+import {IERC2981, IERC165} from "lib/openzeppelin-contracts/contracts/interfaces/IERC2981.sol";
+import {IOwnable} from "../../src/interfaces/IOwnable.sol";
+import {ERC721AC} from "lib/creator-token-contracts/contracts/erc721c/ERC721AC.sol";
+
+contract Cre8orTestBase {
+    Cre8ors public cre8orsNFTBase;
+    DummyMetadataRenderer public dummyRenderer = new DummyMetadataRenderer();
+    address public constant DEFAULT_OWNER_ADDRESS = address(0x23499);
+    address payable public constant DEFAULT_FUNDS_RECIPIENT_ADDRESS =
+        payable(address(0x21303));
+    uint64 DEFAULT_EDITION_SIZE = 10_000;
+    uint16 DEFAULT_ROYALTY_BPS = 888;
+
+    function cre8orSetup() public {
+        cre8orsNFTBase = new Cre8ors({
+            _contractName: "CRE8ORS",
+            _contractSymbol: "CRE8",
+            _initialOwner: DEFAULT_OWNER_ADDRESS,
+            _fundsRecipient: payable(DEFAULT_FUNDS_RECIPIENT_ADDRESS),
+            _editionSize: DEFAULT_EDITION_SIZE,
+            _royaltyBPS: DEFAULT_ROYALTY_BPS,
+            _metadataRenderer: dummyRenderer,
+            _salesConfig: IERC721Drop.SalesConfiguration({
+                publicSaleStart: 0,
+                erc20PaymentToken: address(0),
+                publicSaleEnd: type(uint64).max,
+                presaleStart: 0,
+                presaleEnd: 0,
+                publicSalePrice: 0,
+                maxSalePurchasePerAddress: 0,
+                presaleMerkleRoot: bytes32(0)
+            })
+        });
+    }
+}
