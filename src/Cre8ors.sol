cc3edbc4edb4be693f769746213614cc21eefcca
Sweets Sweetman <sweetmantech@gmail.com>
2023-07-18 17:26:17 -0300

    I remove payToUnlock.

--- src/Cre8ors.sol
+++ src/Cre8ors.sol
@@ -10,7 +10,6 @@ import {MerkleProof} from "@openzeppelin/contracts/utils/cryptography/MerkleProo
 import {IERC721Drop} from "./interfaces/IERC721Drop.sol";
 import {IMetadataRenderer} from "./interfaces/IMetadataRenderer.sol";
 import {ILockup} from "./interfaces/ILockup.sol";
-import {IPayToUnlock} from "./interfaces/IPayToUnlock.sol";
 import {ERC721DropStorageV1} from "./storage/ERC721DropStorageV1.sol";
 import {OwnableSkeleton} from "./utils/OwnableSkeleton.sol";
 import {IOwnable} from "./interfaces/IOwnable.sol";
@@ -44,9 +43,6 @@ contract Cre8ors is
 
     /// @dev Lockup Contract
     ILockup public lockup;
-
-    /// @dev PayToUnlock Contract
-    IPayToUnlock public payToUnlock;
 
     constructor(
         string memory _contractName,
@@ -439,12 +435,6 @@ contract Cre8ors is
     /// @param newLockup new lockup address to use
     function setLockup(ILockup newLockup) external onlyAdmin {
         lockup = newLockup;
-    }
-
-    /// @notice Set a new pay to unlock contract
-    /// @param newPayToUnlock new pay to unlock to use
-    function setPayToUnlock(IPayToUnlock newPayToUnlock) external onlyAdmin {
-        payToUnlock = newPayToUnlock;
     }
 
     /// @notice Lockup unlocked verification
