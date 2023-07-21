01f96f49cb77f5c68f2b26a703d8779ea4869c09
Sweets Sweetman <sweetmantech@gmail.com>
2023-07-21 12:47:16 -0300

    I've added events and NATSPEC doc fixes.

--- src/ERC721ACH.sol
+++ src/ERC721ACH.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.15;
 
 import {ERC721AC} from "ERC721C/erc721c/ERC721AC.sol";
+import {IERC721A} from "erc721a/contracts/IERC721A.sol";
 
 /**
  * @title ERC721ACH
@@ -10,6 +11,73 @@ import {ERC721AC} from "ERC721C/erc721c/ERC721AC.sol";
  *         allows the contract owner to override hooks associated with core ERC721 functions.
  */
 contract ERC721ACH is ERC721AC {
+    /// @notice Emitted when balanceOf hook is used
+    /// @param owner The owner of the tokens
+    /// @param balance The balance of the owner
+    event BalanceOfHookUsed(address indexed owner, uint256 balance);
+
+    /// @notice Emitted when ownerOf hook is used
+    /// @param tokenId The ID of the token
+    /// @param owner The owner of the token
+    event OwnerOfHookUsed(uint256 indexed tokenId, address indexed owner);
+
+    /// @notice Emitted when approve hook is used
+    /// @param approved The address that got approved
+    /// @param tokenId The token ID that got approved
+    event ApproveHookUsed(address indexed approved, uint256 indexed tokenId);
+
+    /// @notice Emitted when setApprovalForAll hook is used
+    /// @param owner The owner of the tokens
+    /// @param operator The operator that got (dis)approved
+    /// @param approved The approval status
+    event SetApprovalForAllHookUsed(
+        address indexed owner,
+        address indexed operator,
+        bool approved
+    );
+
+    /// @notice Emitted when getApproved hook is used
+    /// @param tokenId The ID of the token
+    /// @param approved The approved address
+    event GetApprovedHookUsed(
+        uint256 indexed tokenId,
+        address indexed approved
+    );
+
+    /// @notice Emitted when isApprovedForAll hook is used
+    /// @param owner The owner of the tokens
+    /// @param operator The operator
+    /// @param isApproved The approval status
+    event IsApprovedForAllHookUsed(
+        address indexed owner,
+        address indexed operator,
+        bool isApproved
+    );
+
+    /// @notice Emitted when transferFrom hook is used
+    /// @param from The sender of the token
+    /// @param to The receiver of the token
+    /// @param tokenId The ID of the token
+    event TransferFromHookUsed(
+        address indexed from,
+        address indexed to,
+        uint256 indexed tokenId
+    );
+
+    /// @notice Emitted when safeTransferFrom hook is used
+    /// @param sender The sender of the token
+    /// @param from The current owner of the token
+    /// @param to The receiver of the token
+    /// @param tokenId The ID of the token
+    /// @param data Additional data
+    event SafeTransferFromHookUsed(
+        address indexed sender,
+        address indexed from,
+        address indexed to,
+        uint256 tokenId,
+        bytes data
+    );
+
     /// @notice Contract constructor
     /// @param _contractName The name for the token contract
     /// @param _contractSymbol The symbol for the token contract
@@ -18,7 +86,7 @@ contract ERC721ACH is ERC721AC {
         string memory _contractSymbol
     ) ERC721AC(_contractName, _contractSymbol) {}
 
-    /// @inheritdoc
+    /// @inheritdoc IERC721A
     function supportsInterface(
         bytes4 interfaceId
     ) public view virtual override returns (bool) {
@@ -29,7 +97,7 @@ contract ERC721ACH is ERC721AC {
     /// ERC721 overrides
     /////////////////////////////////////////////////
 
-    /// @inheritdoc
+    /// @inheritdoc IERC721A
     function balanceOf(
         address owner
     ) public view virtual override returns (uint256) {
@@ -39,7 +107,7 @@ contract ERC721ACH is ERC721AC {
         return super.balanceOf(owner);
     }
 
-    /// @inheritdoc
+    /// @inheritdoc IERC721A
     function ownerOf(
         uint256 tokenId
     ) public view virtual override returns (address) {
@@ -49,31 +117,33 @@ contract ERC721ACH is ERC721AC {
         return super.ownerOf(tokenId);
     }
 
-    /// @inheritdoc
+    /// @inheritdoc IERC721A
     function approve(
         address approved,
         uint256 tokenId
     ) public payable virtual override {
         if (_useApproveHook(approved, tokenId)) {
+            emit ApproveHookUsed(approved, tokenId);
             _approveHook(approved, tokenId);
         } else {
             super.approve(approved, tokenId);
         }
     }
 
-    /// @inheritdoc
+    /// @inheritdoc IERC721A
     function setApprovalForAll(
         address operator,
         bool approved
     ) public virtual override {
         if (_useSetApprovalForAllHook(msg.sender, operator, approved)) {
+            emit SetApprovalForAllHookUsed(msg.sender, operator, approved);
             _setApprovalForAllHook(msg.sender, operator, approved);
         } else {
             super.setApprovalForAll(operator, approved);
         }
     }
 
-    /// @inheritdoc
+    /// @inheritdoc IERC721A
     function getApproved(
         uint256 tokenId
     ) public view virtual override returns (address) {
@@ -83,7 +153,7 @@ contract ERC721ACH is ERC721AC {
         return super.getApproved(tokenId);
     }
 
-    /// @inheritdoc
+    /// @inheritdoc IERC721A
     function isApprovedForAll(
         address owner,
         address operator
@@ -94,20 +164,21 @@ contract ERC721ACH is ERC721AC {
         return super.isApprovedForAll(owner, operator);
     }
 
-    /// @inheritdoc
+    /// @inheritdoc IERC721A
     function transferFrom(
         address from,
         address to,
         uint256 tokenId
     ) public payable virtual override {
         if (_useTransferFromHook(from, to, tokenId)) {
+            emit TransferFromHookUsed(from, to, tokenId);
             _transferFromHook(from, to, tokenId);
         } else {
             super.transferFrom(from, to, tokenId);
         }
     }
 
-    /// @inheritdoc
+    /// @inheritdoc IERC721A
     function safeTransferFrom(
         address from,
         address to,
@@ -115,19 +186,21 @@ contract ERC721ACH is ERC721AC {
         bytes memory data
     ) public payable virtual override {
         if (_useSafeTransferFromHook(msg.sender, from, to, tokenId, data)) {
+            emit SafeTransferFromHookUsed(msg.sender, from, to, tokenId, data);
             _safeTransferFromHook(msg.sender, from, to, tokenId, data);
         } else {
             super.safeTransferFrom(from, to, tokenId, data);
         }
     }
 
-    /// @inheritdoc
+    /// @inheritdoc IERC721A
     function safeTransferFrom(
         address from,
         address to,
         uint256 tokenId
     ) public payable virtual override {
         if (_useSafeTransferFromHook(msg.sender, from, to, tokenId, "")) {
+            emit SafeTransferFromHookUsed(msg.sender, from, to, tokenId, "");
             _safeTransferFromHook(msg.sender, from, to, tokenId, "");
         } else {
             super.safeTransferFrom(from, to, tokenId);
@@ -141,22 +214,30 @@ contract ERC721ACH is ERC721AC {
     /// @notice balanceOf Hook for custom implementation.
     /// @param owner The address to query the balance of
     /// @dev Returns the balance of the specified address
-    function _balanceOfHook(address) internal view virtual returns (uint256) {}
+    function _balanceOfHook(
+        address owner
+    ) internal view virtual returns (uint256) {}
 
     /// @notice Check if the balanceOf function should use hook.
     /// @param owner The address to query the balance of
     /// @dev Returns whether or not to use the hook for balanceOf function
-    function _useBalanceOfHook(address) internal view virtual returns (bool) {}
+    function _useBalanceOfHook(
+        address owner
+    ) internal view virtual returns (bool) {}
 
     /// @notice ownerOf Hook for custom implementation.
     /// @param tokenId The token ID to query the owner of
     /// @dev Returns the owner of the specified token ID
-    function _ownerOfHook(uint256) internal view virtual returns (address) {}
+    function _ownerOfHook(
+        uint256 tokenId
+    ) internal view virtual returns (address) {}
 
     /// @notice Check if the ownerOf function should use hook.
     /// @param tokenId The token ID to query the owner of
     /// @dev Returns whether or not to use the hook for ownerOf function
-    function _useOwnerOfHook(uint256) internal view virtual returns (bool) {}
+    function _useOwnerOfHook(
+        uint256 tokenId
+    ) internal view virtual returns (bool) {}
 
     /// @notice approve Hook for custom implementation.
     /// @param approved The address to be approved for the given token ID
@@ -168,8 +249,8 @@ contract ERC721ACH is ERC721AC {
     /// @param tokenId The token ID to be approved
     /// @dev Returns whether or not to use the hook for approve function
     function _useApproveHook(
-        address,
-        uint256
+        address approved,
+        uint256 tokenId
     ) internal view virtual returns (bool) {}
 
     /// @notice setApprovalForAll Hook for custom implementation.
