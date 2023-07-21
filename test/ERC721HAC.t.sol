dc4531993dd965feca18aa3e76098f36346f9c47
Sweets Sweetman <sweetmantech@gmail.com>
2023-07-21 10:53:16 -0300

    I add fuzzing variable: _tokenToApprove in the test test_approve.

--- test/ERC721HAC.t.sol
+++ test/ERC721HAC.t.sol
@@ -52,22 +52,55 @@ contract ERC721ACHTest is DSTest {
         assertEq(erc721Mock.ownerOf(_mintQuantity), address(0));
     }
 
-    function test_approve(uint256 _mintQuantity) public {
+    function test_approve(
+        uint256 _mintQuantity,
+        uint256 _tokenToApprove
+    ) public {
         vm.assume(_mintQuantity > 0);
+        vm.assume(_tokenToApprove > 0);
         vm.assume(_mintQuantity < 10_000);
+        vm.assume(_tokenToApprove <= _mintQuantity);
 
         // Mint some tokens first
         erc721Mock.mint(DEFAULT_BUYER_ADDRESS, _mintQuantity);
 
         // Verify normal functionality
+        assertEq(address(0), erc721Mock.getApproved(_tokenToApprove));
         vm.prank(DEFAULT_BUYER_ADDRESS);
-        erc721Mock.approve(DEFAULT_OWNER_ADDRESS, _mintQuantity);
-        assertEq(DEFAULT_OWNER_ADDRESS, erc721Mock.getApproved(_mintQuantity));
+        erc721Mock.approve(DEFAULT_OWNER_ADDRESS, _tokenToApprove);
+        assertEq(
+            DEFAULT_OWNER_ADDRESS,
+            erc721Mock.getApproved(_tokenToApprove)
+        );
 
         // Verify hook override
         erc721Mock.setHooksEnabled(true);
         vm.expectRevert(ERC721HACMock.Hook_Executed.selector);
         vm.prank(DEFAULT_BUYER_ADDRESS);
-        erc721Mock.approve(DEFAULT_OWNER_ADDRESS, _mintQuantity);
+        erc721Mock.approve(DEFAULT_OWNER_ADDRESS, _tokenToApprove);
     }
+
+    // function test_setApprovalForAll(uint256 _mintQuantity) public {
+    //     vm.assume(_mintQuantity > 0);
+    //     vm.assume(_mintQuantity < 10_000);
+
+    //     // Mint some tokens first
+    //     erc721Mock.mint(DEFAULT_BUYER_ADDRESS, _mintQuantity);
+
+    //     // Verify normal functionality
+    //     vm.prank(DEFAULT_BUYER_ADDRESS);
+    //     erc721Mock.setApprovalForAll(DEFAULT_OWNER_ADDRESS, true);
+    //     assertTrue(
+    //         erc721Mock.isApprovedForAll(
+    //             DEFAULT_BUYER_ADDRESS,
+    //             DEFAULT_OWNER_ADDRESS
+    //         )
+    //     );
+
+    //     // Verify hook override
+    //     erc721Mock.setHooksEnabled(true);
+    //     vm.expectRevert(ERC721HACMock.Hook_Executed.selector);
+    //     vm.prank(DEFAULT_BUYER_ADDRESS);
+    //     erc721Mock.setApprovalForAll(DEFAULT_OWNER_ADDRESS, true);
+    // }
 }
