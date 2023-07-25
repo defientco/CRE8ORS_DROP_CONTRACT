3f813c442f89c88455789efcf6d87760edf222c8
Sameer Rasa <43687814+crazzywizard@users.noreply.github.com>
2023-07-24 21:45:33 -0400

    Update src/interfaces/ICre8ors.sol
    Co-authored-by: sweetman.eth <sweetmantech@gmail.com>

--- src/interfaces/ICre8ors.sol
+++ src/interfaces/ICre8ors.sol
@@ -16,9 +16,4 @@ import {ILockup} from "./ILockup.sol";
 interface ICre8ors is IERC721Drop {
     function lockup() external view returns (ILockup);
 
-    /// @notice This is an admin mint function to mint a quantity to a specific address
-    /// @param to address to mint to
-    /// @param quantity quantity to mint
-    /// @return the id of the first minted NFT
-    function adminMint(address to, uint256 quantity) external returns (uint256);
 }
