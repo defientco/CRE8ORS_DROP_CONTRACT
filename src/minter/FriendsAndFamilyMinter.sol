3925c518bd9ecc73ff954d34dd46d2110b754111
Ahmed Ali <imahmedalibhatti@gmail.com>
2023-08-03 23:16:56 +0500

    feat: integrate subscription contract in FriendsAndFamily.sol

--- src/minter/FriendsAndFamilyMinter.sol
+++ src/minter/FriendsAndFamilyMinter.sol
@@ -7,8 +7,11 @@ import {IERC721Drop} from "../interfaces/IERC721Drop.sol";
 import {ILockup} from "../interfaces/ILockup.sol";
 import {IMinterUtilities} from "../interfaces/IMinterUtilities.sol";
 import {IFriendsAndFamilyMinter} from "../interfaces/IFriendsAndFamilyMinter.sol";
+import {ISubscription} from "../subscription/interfaces/ISubscription.sol";
 
 contract FriendsAndFamilyMinter is IFriendsAndFamilyMinter {
+    uint64 public constant ONE_YEAR_DURATION = 365 days;
+
     ///@notice Mapping to track whether an address has discount for free mint.
     mapping(address => bool) public hasDiscount;
 
@@ -17,12 +20,16 @@ contract FriendsAndFamilyMinter is IFriendsAndFamilyMinter {
     ///@notice The address of the minter utility contract that contains shared utility info.
     address public minterUtilityContractAddress;
 
+    /// @dev The address of the subscription contract.
+    address public subscription;
+
     ///@notice mapping of address to quantity of free mints claimed.
     mapping(address => uint256) public totalClaimed;
 
-    constructor(address _cre8orsNFT, address _minterUtilityContractAddress) {
+    constructor(address _cre8orsNFT, address _minterUtilityContractAddress, address _subscription) {
         cre8orsNFT = _cre8orsNFT;
         minterUtilityContractAddress = _minterUtilityContractAddress;
+        subscription = _subscription;
     }
 
     /// @dev Mints a new token for the specified recipient and performs additional actions, such as setting the lockup (if applicable).
@@ -31,6 +38,14 @@ contract FriendsAndFamilyMinter is IFriendsAndFamilyMinter {
     function mint(address recipient) external onlyExistingDiscount(recipient) returns (uint256) {
         // Mint the token
         uint256 pfpTokenId = ICre8ors(cre8orsNFT).adminMint(recipient, 1);
+
+        // Subscribe for 1 year
+        ISubscription(subscription).updateSubscriptionForFree({
+            target: cre8orsNFT,
+            duration: ONE_YEAR_DURATION,
+            tokenId: pfpTokenId
+        });
+
         totalClaimed[recipient] += 1;
 
         // Reset discount for the recipient
@@ -69,6 +84,14 @@ contract FriendsAndFamilyMinter is IFriendsAndFamilyMinter {
     /// @param _newMinterUtilityContractAddress The address of the new MinterUtilities contract.
     function setNewMinterUtilityContractAddress(address _newMinterUtilityContractAddress) external onlyAdmin {
         minterUtilityContractAddress = _newMinterUtilityContractAddress;
+    }
+
+    function setSubscription(address newSubscription) external onlyAdmin {
+        if (newSubscription == address(0)) {
+            revert ISubscription.SubscriptionCannotBeZeroAddress();
+        }
+
+        subscription = newSubscription;
     }
 
     /// @dev Modifier that restricts access to only the contract's admin.
