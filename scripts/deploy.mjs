2c8b61f56b0660c43fa89a2c35a7bbcec26f6225
Sweets Sweetman <sweetmantech@gmail.com>
2023-08-05 18:57:18 -0300

    I rollback comments.

--- scripts/deploy.mjs
+++ scripts/deploy.mjs
@@ -21,51 +21,52 @@ export async function setupContracts() {
   console.log("deploying...");
   const presaleMerkleRoot =
     "0x36c161febf4b54734baf31a4d6b00da9f4a1cc6eeae64bb328e095b1ab00ec96";
-  // const subscription = await deploySubscription(cre8ors.deploy.deployedTo);
-  await deployInitializer();
+  const subscription = await deploySubscription(cre8ors.deploy.deployedTo);
+  const initializer = await deployInitializer();
   const cre8ors = await deployCre8ors(presaleMerkleRoot);
-  // const hooks = await deployTransfers(cre8ors.deploy.deployedTo);
-  // const staking = await deployStaking();
-  // const lockup = await deployLockup();
-  // const passportAddress = "0x31E28672F704d6F8204e41Ec0B93EE2b1172558E";
+  const hooks = await deployTransfers(cre8ors.deploy.deployedTo);
+  const staking = await deployStaking();
+  const lockup = await deployLockup();
+  const passportAddress = "0x31E28672F704d6F8204e41Ec0B93EE2b1172558E";
 
-  // const minterUtilities = await deployMinterUtilities(
-  //   cre8ors.deploy.deployedTo
-  // );
-  // const familyFriendsMinter = await deployFamilyAndFriendsMinter(
-  //   cre8ors.deploy.deployedTo,
-  //   minterUtilities.deploy.deployedTo
-  // );
-  // const passportMinter = await deployPassportMinter(
-  //   cre8ors.deploy.deployedTo,
-  //   passportAddress,
-  //   minterUtilities.deploy.deployedTo,
-  //   familyFriendsMinter.deploy.deployedTo
-  // );
-  // const allowlistMinter = await deployAllowlistMinter(
-  //   cre8ors.deploy.deployedTo,
-  //   minterUtilities.deploy.deployedTo,
-  //   passportMinter.deploy.deployedTo,
-  //   familyFriendsMinter.deploy.deployedTo
-  // );
-  // const publicMinter = await deployPublicMinter(
-  //   cre8ors.deploy.deployedTo,
-  //   minterUtilities.deploy.deployedTo,
-  //   passportMinter.deploy.deployedTo,
-  //   familyFriendsMinter.deploy.deployedTo
-  // );
-  // return {
-  //   allowlistMinter,
-  //   cre8ors,
-  //   familyFriendsMinter,
-  //   hooks,
-  //   lockup,
-  //   minterUtilities,
-  //   passportMinter,
-  //   publicMinter,
-  //   staking,
-  //   subscription,
-  // };
+  const minterUtilities = await deployMinterUtilities(
+    cre8ors.deploy.deployedTo
+  );
+  const familyFriendsMinter = await deployFamilyAndFriendsMinter(
+    cre8ors.deploy.deployedTo,
+    minterUtilities.deploy.deployedTo
+  );
+  const passportMinter = await deployPassportMinter(
+    cre8ors.deploy.deployedTo,
+    passportAddress,
+    minterUtilities.deploy.deployedTo,
+    familyFriendsMinter.deploy.deployedTo
+  );
+  const allowlistMinter = await deployAllowlistMinter(
+    cre8ors.deploy.deployedTo,
+    minterUtilities.deploy.deployedTo,
+    passportMinter.deploy.deployedTo,
+    familyFriendsMinter.deploy.deployedTo
+  );
+  const publicMinter = await deployPublicMinter(
+    cre8ors.deploy.deployedTo,
+    minterUtilities.deploy.deployedTo,
+    passportMinter.deploy.deployedTo,
+    familyFriendsMinter.deploy.deployedTo
+  );
+  return {
+    allowlistMinter,
+    cre8ors,
+    familyFriendsMinter,
+    hooks,
+    initializer,
+    lockup,
+    minterUtilities,
+    passportMinter,
+    publicMinter,
+    staking,
+    subscription,
+  };
 }
 
 async function main() {
