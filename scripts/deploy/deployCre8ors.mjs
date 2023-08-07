7432a853bbc9178ffc4812d276ed2c955ed41d89
Sweets Sweetman <sweetmantech@gmail.com>
2023-08-06 21:59:13 -0300

    I fix deploy script merge conflicts.

--- scripts/deploy/deployCre8ors.mjs
+++ scripts/deploy/deployCre8ors.mjs
@@ -1,36 +1,29 @@
-import { retryDeploy, retryVerify } from '../contract.mjs';
-import dotenv from 'dotenv';
+import { retryDeploy, retryVerify } from "../contract.mjs";
+import dotenv from "dotenv";
 
 dotenv.config({
-  path: `.env.${process.env.CHAIN}`
+  path: `.env.${process.env.CHAIN}`,
 });
 
 export async function deployCre8ors(merkleRoot) {
-  console.log('deploying Cre8ors');
-  const contractName = 'cre8ors';
-  const contractSymbol = 'CRE8';
-  const _initialOwner = '0x4D977d9aEceC3776DD73F2f9080C9AF3BC31f505'; // cre8ors.eth
-  const _fundsRecipient = '0xcfBf34d385EA2d5Eb947063b67eA226dcDA3DC38'; // sweetman.eth
-  const _editionSize = '8888';
-  const _royaltyBPS = '888';
-  const publicSalePrice = '150000000000000000';
-  const erc20PaymentToken = '0x0000000000000000000000000000000000000000';
+  console.log("deploying Cre8ors");
+  const contractName = "cre8ors";
+  const contractSymbol = "CRE8";
+  const _initialOwner = "0x4D977d9aEceC3776DD73F2f9080C9AF3BC31f505"; // cre8ors.eth
+  const _fundsRecipient = "0xcfBf34d385EA2d5Eb947063b67eA226dcDA3DC38"; // sweetman.eth
+  const _editionSize = "8888";
+  const _royaltyBPS = "888";
+  const publicSalePrice = "150000000000000000";
+  const erc20PaymentToken = "0x0000000000000000000000000000000000000000";
   const maxSalePurchasePerAddress = 18;
-<<<<<<< HEAD
-  const presaleStart = '18446744073709551615'; // never
-  const presaleEnd = '18446744073709551615'; // forever
-  const publicSaleStart = '18446744073709551615'; // never
-  const publicSaleEnd = '18446744073709551615'; // forever
-=======
-  const presaleStart = "18446744073709551615"; // Saturday, August 5, 2023 12:55:00 PM PM ET
+  const presaleStart = "18446744073709551615"; // never
   const presaleEnd = "18446744073709551615"; // forever
-  const publicSaleStart = "18446744073709551615"; // Saturday, August 5, 2023 1:05:00 PM  ET
+  const publicSaleStart = "18446744073709551615"; // never
   const publicSaleEnd = "18446744073709551615"; // forever
->>>>>>> 2821e149d30c13261fe6b441ae7cfbdde7695a4b
   const presaleMerkleRoot = merkleRoot;
   const _salesConfig = `"(${publicSalePrice},${erc20PaymentToken},${maxSalePurchasePerAddress},${publicSaleStart},${publicSaleEnd},${presaleStart},${presaleEnd},${presaleMerkleRoot})"`;
-  const _metadataRenderer = '0x209511E9fe3c526C61B7691B9308830C1d1612bE'; // from Zora
-  const contractLocation = 'src/Cre8ors.sol:Cre8ors';
+  const _metadataRenderer = "0x209511E9fe3c526C61B7691B9308830C1d1612bE"; // from Zora
+  const contractLocation = "src/Cre8ors.sol:Cre8ors";
   const args = [
     contractName,
     contractSymbol,
@@ -39,7 +32,7 @@ export async function deployCre8ors(merkleRoot) {
     _editionSize,
     _royaltyBPS,
     _salesConfig,
-    _metadataRenderer
+    _metadataRenderer,
   ];
   const dropContract = await retryDeploy(2, contractLocation, args);
   console.log(`[deployed] ${contractLocation}`);
@@ -52,7 +45,7 @@ export async function deployCre8ors(merkleRoot) {
     publicSaleEnd,
     presaleStart,
     presaleEnd,
-    presaleMerkleRoot
+    presaleMerkleRoot,
   ];
   const args2 = [
     contractName,
@@ -62,13 +55,13 @@ export async function deployCre8ors(merkleRoot) {
     _editionSize,
     _royaltyBPS,
     _salesConfig2,
-    _metadataRenderer
+    _metadataRenderer,
   ];
   const dropContractAddress = dropContract.deploy.deployedTo;
 
   await retryVerify(2, dropContractAddress, contractLocation, args2);
   console.log(`[verified] ${contractLocation}`);
-  console.log('deployed cre8ors to ', dropContractAddress);
+  console.log("deployed cre8ors to ", dropContractAddress);
 
   return dropContract;
 }
