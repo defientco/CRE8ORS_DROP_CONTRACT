ffd331c3922acc8d60f4d0e2b955f73f1b764584
Sweets Sweetman <sweetmantech@gmail.com>
2023-08-08 06:11:37 -0300

    I update comment in Transfer deploy script to explain where to find latest deployments of ERC6551 contracts.:

--- scripts/deploy/deployTransfers.mjs
+++ scripts/deploy/deployTransfers.mjs
@@ -8,8 +8,8 @@ dotenv.config({
 export async function deployTransfers(cre8orsNftAddress) {
   console.log("deploying Transfer Hook");
   const contractLocation = "src/hooks/Transfers.sol:TransferHook";
-  const ERC6551Registry = "0x02101dfB77FDE026414827Fdc604ddAF224F0921";
-  const ERC6551Implementation = "0x2D25602551487C3f3354dD80D76D54383A243358";
+  const ERC6551Registry = "0x02101dfB77FDE026414827Fdc604ddAF224F0921"; // https://docs.tokenbound.org/contracts/deployments#registry
+  const ERC6551Implementation = "0x2D25602551487C3f3354dD80D76D54383A243358"; // https://docs.tokenbound.org/contracts/deployments#account-implementation
   const args = [cre8orsNftAddress, ERC6551Registry, ERC6551Implementation];
   const contract = await deployAndVerify(contractLocation, args);
   const contractAddress = contract.deployed.deploy.deployedTo;
