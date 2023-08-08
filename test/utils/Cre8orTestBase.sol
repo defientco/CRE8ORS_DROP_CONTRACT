7aea8883b3868aab9f0efdd2570ffacbbb6bfd5a
Sweets Sweetman <sweetmantech@gmail.com>
2023-08-08 05:26:29 -0300

    I DRY setup.

--- test/utils/Cre8orTestBase.sol
+++ test/utils/Cre8orTestBase.sol
@@ -35,25 +35,7 @@ contract Cre8orTestBase is Test {
     Account erc6551Implementation;
 
     function cre8orSetup() public {
-        cre8orsNFTBase = new Cre8ors({
-            _contractName: "CRE8ORS",
-            _contractSymbol: "CRE8",
-            _initialOwner: DEFAULT_OWNER_ADDRESS,
-            _fundsRecipient: payable(DEFAULT_FUNDS_RECIPIENT_ADDRESS),
-            _editionSize: DEFAULT_EDITION_SIZE,
-            _royaltyBPS: DEFAULT_ROYALTY_BPS,
-            _metadataRenderer: dummyRenderer,
-            _salesConfig: IERC721Drop.SalesConfiguration({
-                publicSaleStart: 0,
-                erc20PaymentToken: address(0),
-                publicSaleEnd: type(uint64).max,
-                presaleStart: 0,
-                presaleEnd: 0,
-                publicSalePrice: 0,
-                maxSalePurchasePerAddress: 0,
-                presaleMerkleRoot: bytes32(0)
-            })
-        });
+        cre8orsNFTBase = _deployCre8or();
     }
 
     function _deployCre8or() internal returns (Cre8ors) {
