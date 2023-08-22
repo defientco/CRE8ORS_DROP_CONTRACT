84a7987223ee1081ad8bba8bdafcb504de0150cc
Sweets Sweetman <sweetmantech@gmail.com>
2023-08-22 14:16:27 -0300

    I update deploy scripts & deploy new version of Transfer Hook.

--- scripts/deploy/deployTransfers.mjs
+++ scripts/deploy/deployTransfers.mjs
@@ -5,10 +5,10 @@ dotenv.config({
   path: `.env.${process.env.CHAIN}`,
 });
 
-export async function deployTransfers(cre8orsNftAddress) {
+export async function deployTransfers(cre8orsNftAddress, stakingAddress) {
   console.log("deploying Transferv0.1 Hook");
   const contractLocation = "src/hooks/Transfersv0_1.sol:TransferHookv0_1";
-  const args = [cre8orsNftAddress];
+  const args = [cre8orsNftAddress, stakingAddress];
   const contract = await deployAndVerify(contractLocation, args);
   const contractAddress = contract.deployed.deploy.deployedTo;
   console.log("deployed transfer hook to ", contractAddress);
@@ -22,4 +22,5 @@ export async function deployTransfers(cre8orsNftAddress) {
 }
 
 const GOERLI_CRE8ORS = "0x68C885f0954094C59847E6FeB252Fe5B4b0451Ba";
-await deployTransfers(GOERLI_CRE8ORS);
+const GOERLI_STAKING = "0x0c0a5BE4119A5f121C5928eAE3f61d09Db3c4a7d";
+await deployTransfers(GOERLI_CRE8ORS, GOERLI_STAKING);
